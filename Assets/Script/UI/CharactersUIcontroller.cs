a7361a3903eb5c21d6a7f1ed9a836ea337809445
Even590 <lance87110612@gmail.com>
2025-03-19 17:46:56 +0800

    optimizeScript

--- Assets/Script/UI/CharactersUIcontroller.cs
+++ Assets/Script/UI/CharactersUIcontroller.cs
@@ -1,18 +1,40 @@
+using System;
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class CharactersUIcontroller : MonoBehaviour
 {
+    //ñ◊ë∂éã‚xìÆ·`
+    public UnityEvent openAction;
+    public UnityEvent closeAction;
+    //ùSéÊåÄèÓUIâÓñ 
+    public DialogueTake dialogue;
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        if (dialogue != null) 
+        {
+            ShowCharactherUI();
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    private void ShowCharactherUI()
+    {
+        StartCoroutine(CharactherUIAnimation());
+    }
+
+    private IEnumerator CharactherUIAnimation()
+    {
+        yield return new WaitForSeconds(1.8f);
+        gameObject.GetComponent<CharactersUIcontroller>().openAction.Invoke();
     }
 }
